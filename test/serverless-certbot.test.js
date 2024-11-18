b19157b7d5e2100afee9194a7ace8118ac467e7d
Lucas Mendes <devsdmf@gmail.com>
2024-11-18 17:37:13 -0300

    Initial commit

---
+++ test/serverless-certbot.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ServerlessCertbot = require('../lib/serverless-certbot-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-certbot-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ServerlessCertbot.ServerlessCertbotStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
